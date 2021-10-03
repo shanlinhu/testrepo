3045ae79cb44920fec0f25f9ff4c5c47ada3c46e
shanlinhu <90803138+shanlinhu@users.noreply.github.com>
2021-10-03 15:02:41 -0700

    Update module2_1.sql

--- module2_1.sql
+++ module2_1.sql
@@ -15,7 +15,7 @@ SELECT MIN(DATE) FROM SPACEXTBL WHERE "Landing _Outcome"='Success (drone ship)';
 
 SELECT MIN(DATE) FROM SPACEXTBL WHERE "Landing _Outcome"='Success (ground pad)';
 
-SELECT MIN(DATE) FROM SPACEXTBL WHERE MISSION_OUTCOME='Success';
+SELECT MIN(DATE) FROM SPACEXTBL WHERE MISSION_OUTCOME LIKE 'Success%';
 
 SELECT colname FROM syscat.columns WHERE tabname = 'SPACEXTBL';
 
